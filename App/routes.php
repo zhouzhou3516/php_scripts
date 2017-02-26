340be8e2b4b0f643c468690c6259fe15afeb711c
zhouzhou3516 <735338750@qq.com>
2017-02-26 18:45:58 +0800

    init composer

--- App/routes.php
+++ App/routes.php
@@ -5,3 +5,26 @@
  * Date: 26/02/2017
  * Time: 6:10 PM
  */
+
+use Slim\App;
+use Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware;
+/***
+ * The slim documents: http://www.slimframework.com/docs/objects/router.html
+ */
+
+// config
+$debug = false;
+if (defined("DEBUG")) {
+    $debug = true;
+}
+// Make a Slim App
+// $app = new App($c)
+$app = new App([
+    'settings' => [
+        'debug' => $debug,
+        'whoops.editor' => 'sublime'
+    ]
+]);
+$app->add(new WhoopsMiddleware);
+// Home
+$app->get('/', 'App\Controllers\HomeController:index');
